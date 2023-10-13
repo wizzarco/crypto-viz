f50036788c0a36566d77374c71f1f24ef373e704
WizzarCo <70769001+wizzarco@users.noreply.github.com>
2023-10-13 16:34:01 +0200

    [UPT] Ajout des différents links back pour afficher les top crypto avec l'api de gecko, pour l'instant en full display pour préparer le front

--- back/api_crypto/server.js
+++ back/api_crypto/server.js
@@ -8,29 +8,79 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors()); 
 
-app.get('/api/cryptos', async (req, res) => {
-  try {
-    const apiKey = 'E78147AA-2CF7-46C8-BFE0-50C5227EEA44'; // Remplacez par votre clé API CoinGecko
-    const response = await axios.get('https://rest.coinapi.io/v1/assets', {
-      params: {
-        apikey: apiKey,
-        limit: 10,
-      },
+// ... (autres importations)
+
+app.get('/api/cryptos/top-trending', async (req, res) => {
+try {
+    const apiKey = 'CG-dM9mQcvNb2ePFwDss4Mip7Pi';
+    const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=gecko_desc&page=1&per_page=3&sparkline=false&locale=en', {
+        params: {
+            apikey: apiKey,
+        },
     });
 
-    const cryptocurrencies = response.data.map(crypto => ({
-      name: crypto.name,
-      symbol: crypto.asset_id,
-      price: crypto.price_usd,
-      market_cap: crypto.market_cap_usd,
+    const topTrendingCryptos = response.data.map(crypto => ({
+        image: crypto.image,
+        name: crypto.name,
+        symbol: crypto.symbol,
+        price: crypto.current_price,
+        market_cap: crypto.market_cap,
     }));
 
-    res.json(cryptocurrencies);
-  } catch (error) {
+    res.json(topTrendingCryptos);
+} catch (error) {
     console.error('Erreur lors de la récupération des données:', error.response ? error.response.data : error.message);
     res.status(error.response ? error.response.status : 500).json({ error: 'Erreur lors de la récupération des données' });
-  }
+}
 });
+
+app.get('/api/cryptos/top-gainers', async (req, res) => {
+    try {
+        const apiKey = 'CG-dM9mQcvNb2ePFwDss4Mip7Pi';
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=price_change_percentage_24h&page=1&per_page=3&sparkline=false&locale=en', {
+            params: {
+                apikey: apiKey,
+            },
+        });
+
+        const topGainersCryptos = response.data.map(crypto => ({
+            image: crypto.image,
+            name: crypto.name,
+            symbol: crypto.symbol,
+            price: crypto.current_price,
+            market_cap: crypto.market_cap,
+        }));
+
+        res.json(topGainersCryptos);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error.response ? error.response.data : error.message);
+        res.status(error.response ? error.response.status : 500).json({ error: 'Erreur lors de la récupération des données' });
+    }
+});
+
+app.get('/api/cryptos/recently-added', async (req, res) => {
+    try {
+        const apiKey = 'CG-dM9mQcvNb2ePFwDss4Mip7Pi';
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=date_added&page=1&per_page=3&sparkline=false&locale=en', {
+            params: {
+                apikey: apiKey,
+            },
+        });
+
+        const recentlyAddedCryptos = response.data.map(crypto => ({
+            image: crypto.image,
+            name: crypto.name,
+            symbol: crypto.symbol,
+            price: crypto.current_price,
+            market_cap: crypto.market_cap,
+        }));
+
+        res.json(recentlyAddedCryptos);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error.response ? error.response.data : error.message);
+        res.status(error.response ? error.response.status : 500).json({ error: 'Erreur lors de la récupération des données' });
+    }
+});  
 
 app.listen(port, () => {
   console.log(`Le serveur est en cours d'exécution sur http://localhost:${port}`);
