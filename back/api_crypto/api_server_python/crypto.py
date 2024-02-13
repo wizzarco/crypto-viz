19a91349c5b4b4eb6f50085e34e2f7876eead2e0
WizzarCo <70769001+wizzarco@users.noreply.github.com>
2024-02-13 11:10:22 +0100

    [UPT] Finalisation du projet complet

--- back/api_crypto/api_server_python/crypto.py
+++ back/api_crypto/api_server_python/crypto.py
@@ -1,13 +1,63 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+import threading
+import time
+from datetime import datetime
 from flask import Flask, jsonify, request
 import requests
+from confluent_kafka import Consumer, KafkaException, Producer
 
 app = Flask(__name__)
 
+# Configuration Kafka
+kafka_config = {
+    'bootstrap.servers': 'kafka-server2:9092'
+}
+
+# Créer un producteur Kafka
+kafka_producer = Producer(kafka_config)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f'Message non envoyé: {err}')
+    else:
+        print(f'Message envoyé à {msg.topic()} [{msg.partition()}]')
+
+# Fonction pour envoyer des messages au topic Kafka
+def send_kafka_message(topic, message):
+    try:
+        kafka_producer.produce(topic, message, callback=delivery_report)
+        kafka_producer.poll(0)
+    except Exception as e:
+        print(f'Erreur lors de l\'envoi du message Kafka: {e}')
+
+def create_kafka_consumer(topic):
+    consumer_config = {
+        'bootstrap.servers': 'kafka-server2:9092',
+        'group.id': 'savebdd-group',
+        'auto.offset.reset': 'earliest'
+    }
+    consumer = Consumer(consumer_config)
+    consumer.subscribe([topic])
+
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaException._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+            print(f'Message reçu: {msg.value().decode("utf-8")}')
+    finally:
+        consumer.close()
+
 # Configuration de Cassandra
 cassandra_config = {
-    'contact_points': ['127.0.0.1'],
+    'contact_points': ['host.docker.internal'],
     'auth_provider': PlainTextAuthProvider(username='loic', password='test1234')
 }
 
@@ -21,88 +71,121 @@ def check_cassandra_connection():
     try:
         # Utiliser la session globale définie lors de la configuration
         result = session.execute("SELECT release_version FROM system.local").one()
-
-        return jsonify({'message': f'Connexion à Cassandra réussie. Version: {result.release_version}'})
+        message = f'Connexion à Cassandra réussie. Version: {result.release_version}'
+        send_kafka_message('save_bdd', message)
+        return jsonify({'message': message})
     except Exception as e:
-        return jsonify({'error': f'Erreur lors de la connexion à Cassandra : {str(e)}'}), 500
+        error_message = f'Erreur lors de la connexion à Cassandra : {str(e)}'
+        send_kafka_message('save_bdd', error_message)
+        return jsonify({'error': error_message}), 500
 
 # Route pour sauvegarder les crypto-monnaies
 @app.route('/api_bdd/enregistrer_donnees_cryptocurrency', methods=['GET'])
 def enregistrer_donnees_cryptocurrency():
     try:
-        # Nombre total de pages
-        total_pages = 10  # Mettez à jour avec le nombre total de pages
-
-        # Itérer sur toutes les pages et enregistrer les données
+        total_pages = 159  # Nombre total de pages à itérer
         for page in range(1, total_pages + 1):
-            # Récupérer les données depuis le serveur Node.js
-            response = requests.get(f'http://localhost:11003/api_back/cryptos/all-cryptocurrency?page={page}')
+            response = requests.get(f'http://host.docker.internal:11003/api_back/cryptos/all-cryptocurrency?page={page}')
+            if response.status_code != 200:
+                error_message = f'Erreur lors de la récupération des données pour la page {page}'
+                send_kafka_message('save_bdd', error_message)
+                return jsonify({'error': error_message}), response.status_code
+            
+            crypto_currency_data = response.json()
+            enregistrer_donnees_cassandra_all('crypto_currency_all', crypto_currency_data)
 
-            # Vérifier si la réponse est valide (code HTTP 200)
-            if response.status_code == 200:
-                crypto_currency_data = response.json()
-
-                # Enregistrer les données dans Cassandra
-                enregistrer_donnees_cassandra('crypto_currency', crypto_currency_data)
-            else:
-                return jsonify({'error': f'Erreur lors de la récupération des données pour la page {page}'}), response.status_code
-
-        return jsonify({'message': 'Données enregistrées avec succès'})
+        success_message = 'Toutes les pages ont été enregistrées avec succès pour les cryptocurrency'
+        send_kafka_message('save_bdd', success_message)
+        return jsonify({'message': success_message})
     except Exception as e:
+        error_message = f'Erreur lors de l\'enregistrement des données: {e}'
+        send_kafka_message('save_bdd', error_message)
         return jsonify({'error': str(e)}), 500
 
-def enregistrer_donnees_cassandra(table_name, data):
-    # Utiliser la session globale définie lors de la configuration
-    session.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ("
-                    "id UUID PRIMARY KEY, "
-                    "image TEXT, "
-                    "symbol TEXT, "
-                    "fullname TEXT, "
-                    "maxsupply FLOAT, "
-                    "top24h TEXT, "
-                    "price TEXT, "
-                    "lastvolume TEXT, "
-                    "volumehour TEXT, "
-                    "volumeday TEXT, "
-                    "volume24h TEXT"
-                    ")")
+def enregistrer_donnees_cassandra_all(table_name, data):
+    session.execute(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id UUID PRIMARY KEY,
+            image TEXT,
+            symbol TEXT,
+            fullname TEXT,
+            maxsupply FLOAT,
+            top24h FLOAT,
+            price FLOAT,
+            lastvolume FLOAT,
+            volumehour FLOAT,
+            volumeday FLOAT,
+            volume24h FLOAT,
+            record_date DATE,
+            record_time TIME
+        )
+    """)
 
-    # Insérer les données dans la table
     for entry in data:
-        session.execute(
-            f"INSERT INTO {table_name} (id, image, symbol, fullname, maxsupply, top24h, price, lastvolume, volumehour, volumeday, volume24h) "
-            "VALUES (uuid(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+        current_date = datetime.now().date()
+        current_time = datetime.now().time()
+
+        maxsupply_value = try_convert_float(entry.get('maxsupply'))
+        top24h_value = try_convert_float(entry.get('top24h'))
+        price_value = try_convert_float(entry.get('price'))
+        lastvolume_value = try_convert_float(entry.get('lastvolume'))
+        volumehour_value = try_convert_float(entry.get('volumehour'))
+        volumeday_value = try_convert_float(entry.get('volumeday'))
+        volume24h_value = try_convert_float(entry.get('volume24h'))
+
+        session.execute(f"""
+            INSERT INTO {table_name} (
+                id, image, symbol, fullname, maxsupply, top24h, price, lastvolume, volumehour, volumeday, volume24h, record_date, record_time
+            ) VALUES (
+                uuid(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            )""",
             (
                 entry.get('image'),
                 entry.get('symbol'),
                 entry.get('fullname'),
-                entry.get('maxsupply'),
-                entry.get('top24h'),
-                entry.get('price'),
-                entry.get('lastvolume'),
-                entry.get('volumehour'),
-                entry.get('volumeday'),
-                entry.get('volume24h'),
-            ),
+                maxsupply_value,
+                top24h_value,
+                price_value,
+                lastvolume_value,
+                volumehour_value,
+                volumeday_value,
+                volume24h_value,
+                current_date,
+                current_time
+            )
         )
+
+def try_convert_float(value, default=0.0):
+    try:
+        return float(value)
+    except ValueError:
+        return default
 
 # Routes de l'API Flask
 @app.route('/api_bdd/enregistrer_donnees_top', methods=['GET'])
 def enregistrer_donnees():
     try:
         # Récupérer les données depuis le serveur Node.js
-        top_trending_data = requests.get('http://localhost:11003/api_back/cryptos/top-trending').json()
-        top_gainers_data = requests.get('http://localhost:11003/api_back/cryptos/top-gainers').json()
-        total_mining_data = requests.get('http://localhost:11003/api_back/cryptos/total-mining').json()
+        top_trending_data = requests.get('http://host.docker.internal:11003/api_back/cryptos/top-trending').json()
+        top_gainers_data = requests.get('http://host.docker.internal:11003/api_back/cryptos/top-gainers').json()
+        total_mining_data = requests.get('http://host.docker.internal:11003/api_back/cryptos/total-mining').json()
  
         # Enregistrer les données dans Cassandra
-        enregistrer_donnees_cassandra('top_trending', top_trending_data)
-        enregistrer_donnees_cassandra('top_gainers', top_gainers_data)
-        enregistrer_donnees_cassandra('total_mining', total_mining_data)
+        success_messages = []
 
+        # Ajouter les messages de succès à la liste
+        success_messages.append(enregistrer_donnees_cassandra('top_trending', top_trending_data))
+        success_messages.append(enregistrer_donnees_cassandra('top_gainers', top_gainers_data))
+        success_messages.append(enregistrer_donnees_cassandra('total_mining', total_mining_data))
+
+        # Joindre tous les messages en une seule chaîne de caractères
+        final_message = ' '.join([msg for msg in success_messages if msg])
+
+        send_kafka_message('save_bdd', f'Données top trending, top gainers, et total mining enregistrées avec succès: {final_message}')
         return jsonify({'message': 'Données enregistrées avec succès'})
 
     except Exception as e:
+        send_kafka_message('save_bdd', f'Erreur lors de l\'enregistrement des données top: {e}')
         return jsonify({'error': str(e)}), 500
 
 def enregistrer_donnees_cassandra(table_name, data):
@@ -121,6 +204,8 @@ def enregistrer_donnees_cassandra(table_name, data):
 
     # Insérer les données dans la table
     for entry in data:
+        if not isinstance(entry, dict):
+            continue
         # Clean up the 'price' value, removing '$' and spaces
         price_raw = entry.get('price')
         cleaned_price = price_raw.replace('$', '').replace(' ', '') if price_raw else None
@@ -179,5 +264,45 @@ def enregistrer_donnees_cassandra(table_name, data):
             ),
         )
 
+# Update les données automatiquement
+def connexion_cassandra():
+    while True:
+        try:
+            requests.get('http://host.docker.internal:11002/api_bdd/check_cassandra_connection')
+
+        except Exception as e:
+            print(f"Erreur lors de la connexion à la base de données : {e}")
+        
+        time.sleep(60000)
+
+def update_data_top():
+    while True:
+        try:
+            requests.get('http://host.docker.internal:11002/api_bdd/enregistrer_donnees_top')
+
+        except Exception as e:
+            print(f"Erreur lors de la mise à jour des données : {e}")
+
+        time.sleep(300)
+    
+def update_data_all():
+    while True:
+        try:
+            requests.get('http://host.docker.internal:11002/api_bdd/enregistrer_donnees_cryptocurrency')
+
+        except Exception as e:
+            print(f"Erreur lors de la mise à jour des données : {e}")
+
+        time.sleep(300)
+
 if __name__ == '__main__':
-    app.run(port=11002)
+    update_thread = threading.Thread(target=connexion_cassandra)
+    update_thread.start()
+
+    update_thread = threading.Thread(target=update_data_top)
+    update_thread.start()
+
+    update_thread = threading.Thread(target=update_data_all)
+    update_thread.start()
+
+    app.run(host='0.0.0.0', port=11002)
--- back/api_crypto/bdd_front.js
+++ back/api_crypto/bdd_front.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { Client } = require('cassandra-driver');
-const { connectProducer, sendToKafka, disconnectProducer } = require('./kafka_api_bdd/KafkaProducer');
-const { connectConsumer, consumeMessages, disconnectConsumer } = require('./kafka_api_bdd/KafkaConsumer');
+// const { connectProducer, sendToKafka, disconnectProducer } = require('./kafka_api_bdd/KafkaProducer');
+// const { connectConsumer, consumeMessages, disconnectConsumer } = require('./kafka_api_bdd/KafkaConsumer');
 
 const app = express();
 const port = process.env.PORT || 11004;
@@ -12,7 +12,8 @@ app.use(cors());
 
 // Configurer la connexion à Cassandra
 const client = new Client({
-    contactPoints: ['host.docker.internal'],
+    // contactPoints: ['host.docker.internal'],
+    contactPoints: ['127.0.0.1'],
     localDataCenter: 'dc1',
     keyspace: 'tdat901',
     credentials: { username: 'loic', password: 'test1234' },
@@ -20,41 +21,64 @@ const client = new Client({
 
 // Se connecter à Cassandra
 client.connect().then(() => console.log('Connecté à Cassandra')).catch(err => console.error('Erreur de connexion à Cassandra', err));
+// connectProducer().then(() => console.log('Producteur Kafka connecté')).catch(e => console.error('Erreur de connexion du producer Kafka:', e));
 
-// Démarrage du Producer Kafka
-connectProducer().then(() => console.log('Producteur Kafka connecté')).catch(e => console.error('Erreur de connexion du producer Kafka:', e));
-
-// Endpoint pour récupérer des données depuis Cassandra
 app.get('/api_front/cryptos/:table', async (req, res) => {
     const { table } = req.params;
     try {
         const result = await client.execute(`SELECT * FROM ${table}`);
-        await sendToKafka('api_bdd_cryptoviz', result.rows);
+        // await sendToKafka('api_bdd_cryptoviz', result.rows);
         res.json(result.rows);
     } catch (error) {
         console.error(`Erreur lors de la récupération des données depuis ${table} :`, error);
-        await sendToKafka('api_bdd_cryptoviz', { error: error.message, table });
+        // await sendToKafka('api_bdd_cryptoviz', { error: error.message, table });
         res.status(500).json({ error: 'Erreur serveur' });
     }
 });
 
-// Démarrage et utilisation du Consommateur Kafka
-connectConsumer().then(() => {
-    console.log('Consommateur Kafka connecté');
-    consumeMessages((data) => {
-      console.log('Message reçu:', data);
-      // Traitez ici les données reçues de Kafka
-    });
-  }).catch(e => console.error('Erreur de connexion du consommateur Kafka:', e));
+app.get('/api_front/crypto_currency_all/all', async (req, res) => {
+    try {
+        const symbol = req.query.symbol;
+        let query;
+
+        if (symbol) {
+            // Utilisez ALLOW FILTERING avec prudence
+            query = `SELECT * FROM crypto_currency_all WHERE symbol = ? ALLOW FILTERING`;
+        } else {
+            query = `SELECT * FROM crypto_currency_all`;
+        }
+
+        const result = await client.execute(query, symbol ? [symbol] : []);
+
+    //   for (const row of result.rows) {
+    //       await sendToKafka('api_bdd_cryptoviz', row);
+    //   }
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error(`Erreur lors de la récupération des données depuis crypto_currency_all :`, error);
+    //   await sendToKafka('api_bdd_cryptoviz', { error: error.message, table: 'crypto_currency_all' });
+        res.status(500).json({ error: 'Erreur serveur' });
+    }
+});
+
+// // Démarrage et utilisation du Consommateur Kafka
+// connectConsumer().then(() => {
+//     console.log('Consommateur Kafka connecté');
+//     consumeMessages((data) => {
+//       console.log('Message reçu:', data);
+//       // Traitez ici les données reçues de Kafka
+//     });
+//   }).catch(e => console.error('Erreur de connexion du consommateur Kafka:', e));
   
-  // Assurez-vous de déconnecter le producteur et le consommateur lors de l'arrêt du serveur
-  process.on('SIGINT', async () => {
-    console.log('Fermeture du producteur et du consommateur Kafka...');
-    await disconnectProducer();
-    await disconnectConsumer();
-    client.shutdown();
-    process.exit(0);
-  });
+//   // Assurez-vous de déconnecter le producteur et le consommateur lors de l'arrêt du serveur
+//   process.on('SIGINT', async () => {
+//     console.log('Fermeture du producteur et du consommateur Kafka...');
+//     await disconnectProducer();
+//     await disconnectConsumer();
+//     client.shutdown();
+//     process.exit(0);
+//   });
 
 // Démarrer le serveur
 app.listen(port, () => {
